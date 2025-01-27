ca4cde52b498d9ffe07a9ce64ea1d8c0791d7b8f
darksuei <chrisraphael576@gmail.com>
2025-01-27 12:10:01 +0100

    feat: adds dapr for mtls

--- node-microservice/index.js
+++ node-microservice/index.js
@@ -1,17 +1,50 @@
 import express from "express";
+import { DaprServer, DaprClient, HttpMethod } from "@dapr/dapr";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
+
+const APP_HOST = "0.0.0.0";
 const APP_PORT = 5000;
 
+const DAPR_HOST = "0.0.0.0";
+const DAPR_PORT = 3500;
+
+const daprServer = new DaprServer({
+  serverHost: APP_HOST,
+  serverPort: APP_PORT,
+  serverHttp: app,
+  clientOptions: { daprHost: DAPR_HOST, daprPort: DAPR_PORT },
+});
+
+daprServer.start().then(() => {
+  console.log(`Server started on ${APP_HOST}:${APP_PORT}`);
+});
+
+const daprClient = new DaprClient({ daprHost: DAPR_HOST, daprPort: DAPR_PORT });
+
+// ROUTES
+
 app.get("/health", (_, res) => {
+  console.log("Received health check request.");
   return res.status(200).json({ message: "Server is up and running" });
 });
 
-app.get("/protected", (_, res) => {
-  return res.status(200).json({ message: "Protected route" });
+app.get("/data", (req, res) => {
+  console.log(`Received headers: ${JSON.stringify(req.headers)}.`);
+  console.log(`Received dapr request for secure data from host: ${req.hostname}.`);
+  return res.status(200).json({ message: "Secure data" });
 });
 
-app.listen(APP_PORT, () => {
-  console.log(`Server is running on port ${APP_PORT}`);
+app.get("/get-data", (_, res) => {
+  const ext_app_id = process.env.EXT_SERVICE_APP_ID;
+  const ext_method = "data";
+
+  console.log(`Calling external service: app-id: ${ext_app_id} method: ${ext_method}.`);
+
+  daprClient.invoker.invoke(ext_app_id, ext_method, HttpMethod.GET).then((response) => {
+    return res.status(200).json(response);
+  });
 });
